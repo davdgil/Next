d01a712366cd6e33646bcbf8277724f5c47887fe
David Gil <64413374+davdgil@users.noreply.github.com>
2023-12-11 22:41:08 +0100

    ds

--- practica-final-react/src/app/components/Registro/signUp.jsx
+++ practica-final-react/src/app/components/Registro/signUp.jsx
@@ -162,7 +162,7 @@ function SignUp() {
                     className="bg-gray-50 border border-gray-300 text-zinc-950 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
                     required
                   />
-                  {errors.birthdate && <span className="text-red-500 text-sm">sdadad</span>}
+                  {errors.birthdate && <span className="text-red-500 text-sm">sdadadf</span>}
                 
               </div>
   
