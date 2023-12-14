d105088d49800b791ff2142983f64c039cfe5d66
David Gil <64413374+davdgil@users.noreply.github.com>
2023-12-14 10:15:23 +0100

    aa

--- practica-final-react/src/app/components/User/navBar.jsx
+++ practica-final-react/src/app/components/User/navBar.jsx
@@ -21,7 +21,7 @@ function UserBar() {
                 <div className="hidden w-full md:block md:w-auto" id="navbar-default">
                     <ul className="font-medium flex flex-col p-4 md:p-0 mt-4  rounded-lg md:flex-row md:space-x-8 rtl:space-x-reverse md:mt-0 md:border-0  dark:bg-gray-800 md:dark:bg-gray-900 dark:border-gray-700">
                         <li>
-                            <Link href="/merchant" passHref>
+                            <Link href="/user" passHref>
                                 <p className="block py-2 px-3 text-red-500 text-2xl rounded hover:bg-gray-100 hover:border-transparent md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 dark:text-white md:dark:hover:text-blue-500 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent">
                                     Ver comercios
                                 </p>
