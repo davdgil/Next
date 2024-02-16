7ca7a87b1f454f0cd0c11e041569beca36ec8d76
David Gil <64413374+davdgil@users.noreply.github.com>
2024-02-16 13:41:32 +0100

    j

---
+++ mvc/app.js
@@ -0,0 +1,16 @@
+const express = require("express")
+const cors = require("cors")
+
+require('dotenv').config();
+
+const app = express();
+
+app.use(cors());
+
+app.use(express.json());
+
+const port = process.env.PORT || 3000;
+
+app.listen(port, () => {
+    console.log("servidor escuchando en el puerto: "+port);
+})
